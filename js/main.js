b3848d2c1b62b78f247e8e08c02e3558a7005ea2
Nir Fakiro <fakironir@gmail.com>
2024-05-29 16:20:26 +0300

    Improv ball function

--- js/main.js
+++ js/main.js
@@ -4,9 +4,18 @@ var currHeight = 100
 
 function onBallClick() {
   const elBall = document.querySelector('div')
-  elBall.style.width = currWidth + 50 + 'px'
-  currWidth += 50
-  elBall.style.height = currHeight + 50 + 'px'
-  currHeight += 50
-  elBall.innerText = currHeight
+  if (currHeight >= 400) {
+    currWidth = 100
+    currHeight = 100
+
+    elBall.innerText = currHeight
+    elBall.style.width = currWidth + 'px'
+    elBall.style.height = currHeight + 'px'
+  } else {
+    elBall.style.width = currWidth + 50 + 'px'
+    currWidth += 50
+    elBall.style.height = currHeight + 50 + 'px'
+    currHeight += 50
+    elBall.innerText = currHeight
+  }
 }
