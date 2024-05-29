450a6e48e5cd7d44cd607b1df387a8de8f51cce7
Nir Fakiro <fakironir@gmail.com>
2024-05-29 15:41:52 +0300

    Implement the onBallClick function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 'use strict'
+var currWidth = 100
+var currHeight = 100
 
 function onBallClick() {
-  console.log('hi')
+  const elBall = document.querySelector('div')
+  elBall.style.width = currWidth + 50 + 'px'
+  currWidth += 50
+  elBall.style.height = currHeight + 50 + 'px'
+  currHeight += 50
+  elBall.innerText = currHeight
 }
